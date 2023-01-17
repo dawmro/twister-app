0283b42b8627a065a66921479a23c5e9f0225cb5
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-17 08:21:38 +0100

    add main screen image

--- twister-app.py
+++ twister-app.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from PIL import ImageTk
 
 # initiallize app
 root = tk.Tk()
@@ -15,6 +15,15 @@ frame1 = tk.Frame(root, width=400, height=400, bg="#ffffff")
 
 # place frame on the page
 frame1.grid(row=0, column=0)
+
+# load image for main screen
+main_screen_img = ImageTk.PhotoImage("file=assets/main_screen.png")
+
+# convert image to widget for frame1
+main_screen_widget = tk.Label1(frame1, image=main_screen_img)
+main_screen_widget.image = main_screen_img
+
+
 
 # run app
 root.mainloop()
