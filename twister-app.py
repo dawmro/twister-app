f37c2870416f55dadd50dbab489ff9a59cb78734
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-17 08:08:54 +0100

    add first frame

--- twister-app.py
+++ twister-app.py
@@ -10,5 +10,11 @@ root.title("Twister App")
 # center app on the screen
 root.eval("tk::PlaceWindow . center")
 
+# create first frame
+frame1 = tk.Frame(root, width=400, height=400, bg="#ffffff")
+
+# place frame on the page
+frame1.grid(row=0, column=0)
+
 # run app
 root.mainloop()
